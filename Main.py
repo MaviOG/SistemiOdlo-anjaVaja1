169e74d5da945efaf6cef99d1b4c80fb3d8edea5
MaviOG <mark.roskaric@student.um.si>
2024-03-07 18:13:48 +0100

    update

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from PIL import Image, ImageDraw, ImageFont
 import os
 import sys
 import configparser
@@ -9,16 +10,28 @@ def main():
     Header= []
     Pozitiv = []
     Negativ =[]
+    PNGFile = "graf.png"
+    PNGData = ""
+
     #Main
     IniFilePath = GetConfig()
     FilePath = GetFile(IniFilePath)
     Header = ReadHeader(FilePath)
     Pozitiv,Negativ = ReadCsvAndReturnValues(Header,FilePath)
+    PNGData = HurwitzevKriterijPNG(Header,Pozitiv,Negativ)
+    CreatePNGAndSave(PNGFile, PNGData,len(Header))
+    Output(FilePath,Header,Optimist(Pozitiv),Pesimist(Negativ),Laplace(Pozitiv,Negativ),Savage(Pozitiv,Negativ),Negativ,Pozitiv,PNGFile)
     
-    Output(FilePath,Header,Optimist(Pozitiv),Pesimist(Negativ),Laplace(Pozitiv,Negativ),Savage(Pozitiv,Negativ),Negativ,Pozitiv)
     
+def CreatePNGAndSave(filename, text,lenght):
+    fnt = ImageFont.truetype('arial.ttf', 40)
+    image = Image.new(mode="RGB", size=((lenght*300),800), color="white")
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), text, font=fnt, fill=(0, 0, 0))
+    image.save(filename)
+    print(f"Image '{filename}' saved successfully.")
     
-    
+
 
 def Savage(Pozitivno,Negativno):
     SavagePozitiv = []
@@ -94,34 +107,48 @@ def GetConfig():
     file_name = 'Config.ini'
     file_path = os.path.join(current_directory, file_name)
     return file_path
-def Output(FilePath,Header,Optimist,Pesimist,Laplace,Savage,Negativ,Pozitiv):
+def Output(FilePath,Header,Optimist,Pesimist,Laplace,Savage,Negativ,Pozitiv,PNGFile):
     print("Izračun osnovnih metod odločanja.")
     print('Prebrana je bila datoteka "'+FilePath.split("\\")[-1]+'.')
     print(f"Optimist: {Optimist}")
     print(f"Pesimist: {Pesimist}")
     print(f"Laplace: {Laplace}")
     print(f"Savage: {Savage}")
+    HurwitzevKriterij(Header,Pozitiv,Negativ)
+    print(f"Graf Hurwitzovega kriterija je bil shranjen v datoteko ('{PNGFile}').")
+
+
+
+def HurwitzevKriterij(Header,Pozitiv,Negativ):
     print("Hurwitzev kriterij:")
     max_length = max(len(header) for header in Header)
-
-            # Calculate the spacing based on the maximum length
-    spacing = max_length + 5  # Adjust the spacing as needed
+    spacing = max_length + 5  
     spacingNumber = max(9, len('h') - 1)
-    first_spacing = max(8, len('h') - 1)  # Adjust spacing for the first column
-
-            # Print the header row with adjusted spacing
-    print("h".ljust(first_spacing), end='')  # Print first column header
-    print(" ".join(header.ljust(spacing) for header in Header[1:]))  # Print other column headers
-    
-    
+    first_spacing = max(8, len('h') - 1)  
+    print("h".ljust(first_spacing), end='')  
+    print(" ".join(header.ljust(spacing) for header in Header[1:])) 
     for y in range(0,11):
         HurwitzevArray = []
         for z in range(len(Header)-1):
             HurwitzevArray.append(Hurwitzev((y/10),Pozitiv[z],Negativ[z]))
         print(str(y/10).ljust(spacingNumber), end='')
-        #print(HurwitzevArray)
         print(" ".join(str(header).ljust(spacing) for header in HurwitzevArray[:]))
 
+def HurwitzevKriterijPNG(Header, Pozitiv, Negativ):
+    result = "Hurwitzev kriterij:\n"
+    max_length = max(len(header) for header in Header)
+    spacing = max_length + 13 
+    spacingNumber = max(13, len('h') - 1)
+    first_spacing = max(13, len('h') - 1)  
+    result += "h".ljust(first_spacing)
+    result += " ".join(header.ljust(spacing-3) for header in Header[1:]) + "\n"
+    for y in range(0, 11):
+        HurwitzevArray = []
+        for z in range(len(Header) - 1):
+            HurwitzevArray.append(Hurwitzev((y / 10), Pozitiv[z], Negativ[z]))
+        result += str(y / 10).ljust(spacingNumber)
+        result += " ".join(str(header).ljust(spacing) for header in HurwitzevArray[:]) + "\n"
+    return result
 
     print()  # Move to the next line
     
